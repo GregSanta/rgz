eb52bd24011ed356cc03081fe0320b1dc8d55330
GregSanta <102510362+GregSanta@users.noreply.github.com>
2022-06-09 23:26:33 +0700

    Add files via upload

---
+++ Cod/css/common.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const showSlider = new Swiper('.showcase_content', {
+  loop: true, 
+  slidesPerView: 3,
+  speed: 1800,
+  centeredSlides: true
+})
+
+document.querySelector('video').playbackRate = 2
+
+
+
+})
